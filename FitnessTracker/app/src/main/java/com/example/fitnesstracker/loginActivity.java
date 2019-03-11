4dad8d9448d6904030268f3be19b50f6754463d5
Zaid Baghal <4283944+zaidsbaghal@users.noreply.github.com>
2019-03-11 10:09:00 -0400

    initial

---
+++ FitnessTracker/app/src/main/java/com/example/fitnesstracker/loginActivity.java
@@ -0,0 +1,32 @@
+package com.example.fitnesstracker;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class loginActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable,2000); // Timeout for the splash screen
+    }
+}
